7a39bb1f115f57d607e668a4b5999673f54a6c11
prajnab <prajna.b18@gmail.com>
2022-10-25 12:49:23 +0530

    Fixed shimmer issue in iOS

--- ios/RTNShimmerManager.mm
+++ ios/RTNShimmerManager.mm
@@ -16,7 +16,7 @@ RCT_ENUM_CONVERTER(FBShimmerDirection, (@{
   @"down": @(FBShimmerDirectionDown),
 }), FBShimmerDirectionRight, integerValue)
 
-RCT_CUSTOM_CONVERTER(CFTimeInterval, CFTimeInterval, [self double:json] / 1000.0) // TODO: This is not working in RTNShimmer.h
+RCT_CUSTOM_CONVERTER(CFTimeInterval, CFTimeInterval, [self double:json] / 1000.0)
 
 @end
 
@@ -30,6 +30,8 @@ RCT_EXPORT_MODULE()
 {
   return [RTNShimmerView new];
 }
+#endif
+
 RCT_REMAP_VIEW_PROPERTY(animating, shimmering, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(shimmeringDirection, FBShimmerDirection)
 RCT_REMAP_VIEW_PROPERTY(duration, shimmeringDuration, CFTimeInterval)
@@ -40,6 +42,5 @@ RCT_REMAP_VIEW_PROPERTY(speed, shimmeringSpeed, CGFloat)
 RCT_REMAP_VIEW_PROPERTY(highlightLength, shimmeringHighlightLength, CGFloat)
 RCT_REMAP_VIEW_PROPERTY(beginFadeDuration, shimmeringBeginFadeDuration, CFTimeInterval)
 RCT_REMAP_VIEW_PROPERTY(endFadeDuration, shimmeringEndFadeDuration, CFTimeInterval)
-#endif
 
 @end
